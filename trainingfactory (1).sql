bef27147deb46c785e74dcb8b6fed8a848f660f8
Mathijs01 <302974175@student.rocmondriaan.nl>
2020-01-06 11:24:25 +0100

    database toegevoegd

--- trainingfactory (1).sql
+++ trainingfactory (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 13 dec 2019 om 12:36
+-- Gegenereerd op: 06 jan 2020 om 11:22
 -- Serverversie: 10.4.8-MariaDB
 -- PHP-versie: 7.3.11
 
@@ -27,15 +27,36 @@ USE `trainingfactory`;
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `gedragsregels`
+--
+
+CREATE TABLE `gedragsregels` (
+  `id` int(11) NOT NULL,
+  `regels` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `gedragsregels`
+--
+
+INSERT INTO `gedragsregels` (`id`, `regels`) VALUES
+(1, 'gedraag je'),
+(2, 'test');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `lesson`
 --
 
 CREATE TABLE `lesson` (
   `id` int(11) NOT NULL,
+  `instructor_id_id` int(11) DEFAULT NULL,
+  `training_id_id` int(11) DEFAULT NULL,
   `time` time NOT NULL,
   `date` date NOT NULL,
   `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `max_persons` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `max_persons` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -54,7 +75,8 @@ CREATE TABLE `migration_versions` (
 --
 
 INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
-('20191213102232', '2019-12-13 11:33:33');
+('20191216090423', '2019-12-16 09:04:32'),
+('20191218125655', '2019-12-18 12:57:10');
 
 -- --------------------------------------------------------
 
@@ -69,9 +91,15 @@ CREATE TABLE `person` (
   `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `preprovision` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dateofbirth` date NOT NULL,
+  `dateofbirth` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `emailadress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `emailaddress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hiring_date` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL,
+  `street` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `place` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -82,7 +110,9 @@ CREATE TABLE `person` (
 
 CREATE TABLE `registration` (
   `id` int(11) NOT NULL,
-  `payment` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `lesson_id_id` int(11) DEFAULT NULL,
+  `person_id_id` int(11) DEFAULT NULL,
+  `payment` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -93,21 +123,61 @@ CREATE TABLE `registration` (
 
 CREATE TABLE `training` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `naam` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `duration` time NOT NULL,
-  `costs` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `costs` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `training`
+--
+
+INSERT INTO `training` (`id`, `naam`, `description`, `duration`, `costs`) VALUES
+(12, 'Stootzak training', 'slaan op een stootzak', '01:30:00', '6.00'),
+(13, 'KickBoxen', 'training door middel van vechten', '01:30:00', '6.00'),
+(14, 'MMA', 'MMA vechten training', '01:30:00', '14.00');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `firstname`) VALUES
+(1, 'admin@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$Bw596OHTKwRsDwS5atQV5etjhCz9BniTHPsiPZhIVpl26kc61aUXe', 'tester1'),
+(2, 'user@gmail.com', '[\"ROLE_USER\"]', '$2y$13$dfEIyZTXf.KG7wSAbgKKD.eO9I9lIbF/4PaPHv4UZ1HAzYDACsUeW', 'tester2'),
+(3, 'tester3@gmail.com', '[\"ROLE_USER\"]', '$2y$13$X08KqIKsT/pq0hxgg6h92.JZUqbez7WpT8.Hktbrni/ONHQ6/.iJ2', 'tester3'),
+(4, 'tester4@gmail.com', '[\"ROLE_USER\"]', '$2y$13$HywySTylGCiceuWpwjwXzOBdsHRGP/O46rd7GojunSM6y5coc5V1W', 'tester4'),
+(5, 'tester5@gmail.com', '[\"ROLE_USER\"]', 'ja', 'tester5');
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
 
 --
+-- Indexen voor tabel `gedragsregels`
+--
+ALTER TABLE `gedragsregels`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexen voor tabel `lesson`
 --
 ALTER TABLE `lesson`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F87474F3AD96791B` (`instructor_id_id`),
+  ADD KEY `IDX_F87474F3909E143A` (`training_id_id`);
 
 --
 -- Indexen voor tabel `migration_versions`
@@ -125,7 +195,9 @@ ALTER TABLE `person`
 -- Indexen voor tabel `registration`
 --
 ALTER TABLE `registration`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_62A8A7A735A24AD0` (`lesson_id_id`),
+  ADD KEY `IDX_62A8A7A7D3728193` (`person_id_id`);
 
 --
 -- Indexen voor tabel `training`
@@ -136,6 +208,12 @@ ALTER TABLE `training`
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
+
+--
+-- AUTO_INCREMENT voor een tabel `gedragsregels`
+--
+ALTER TABLE `gedragsregels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT voor een tabel `lesson`
@@ -159,7 +237,31 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT voor een tabel `training`
 --
 ALTER TABLE `training`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT voor een tabel `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `lesson`
+--
+ALTER TABLE `lesson`
+  ADD CONSTRAINT `FK_F87474F3909E143A` FOREIGN KEY (`training_id_id`) REFERENCES `training` (`id`),
+  ADD CONSTRAINT `FK_F87474F3AD96791B` FOREIGN KEY (`instructor_id_id`) REFERENCES `person` (`id`);
+
+--
+-- Beperkingen voor tabel `registration`
+--
+ALTER TABLE `registration`
+  ADD CONSTRAINT `FK_62A8A7A735A24AD0` FOREIGN KEY (`lesson_id_id`) REFERENCES `lesson` (`id`),
+  ADD CONSTRAINT `FK_62A8A7A7D3728193` FOREIGN KEY (`person_id_id`) REFERENCES `person` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
